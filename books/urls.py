2c01dec0eca91367f18de74131df4c0864b69f34
jamoliddin007 <jamoliddin_007>
2025-05-19 23:46:01 +0500

    Updated URL config and settings for books app

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import BookListView
+from .api_views import BookListAPIView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="home"),
+    path("books/", BookListAPIView.as_view(), name="book_list_api"),
 ]
+
